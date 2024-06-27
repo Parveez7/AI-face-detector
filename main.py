331ca137fbd6aa83bbe967cbe5f472f74d380f5b
Parveez7 <abdulparveezya@gmail.com>
2024-06-27 11:36:25 +0530

    Tested on a sample face

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv2.imread('sample_face.png')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+face_cordinataes = trained_face_data.detectMultiScale(gray_img)
+for (x, y, w, h) in face_cordinataes:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    break
+cv2.imshow('Face detector', img)
+cv2.waitKey(0)

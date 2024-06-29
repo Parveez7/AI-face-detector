4b5ea6465e3fe6f024f6ce52dd57d4172d3d2117
Parveez7 <abdulparveezya@gmail.com>
2024-06-29 14:41:49 +0530

    commit - works with camera

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
+
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img = cv2.imread('sample_face.png')
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-face_cordinataes = trained_face_data.detectMultiScale(gray_img)
-for (x, y, w, h) in face_cordinataes:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
-    break
-cv2.imshow('Face detector', img)
-cv2.waitKey(0)
+webcam=cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read, frame = webcam.read()
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_cordinataes = trained_face_data.detectMultiScale(gray_img)
+    for (x, y, w, h) in face_cordinataes:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow('Face detector', frame)
+    cv2.waitKey(1)
